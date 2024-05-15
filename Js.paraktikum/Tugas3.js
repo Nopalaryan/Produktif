73b41246df755313e5949262ac946f03deff2844
Nopalaryan <168519161+Nopalaryan@users.noreply.github.com>
2024-05-15 12:32:34 +0700

    Add files via upload

---
+++ Js.paraktikum/Tugas3.js
@@ -0,0 +1,19 @@
+var x="farel"
+x+=15;
+console.log(x)
+
+var x="dimas"
+x+=15;
+console.log(x)
+
+var x="radit"
+x+=15;
+console.log(x)
+
+var x="nopal"
+x+=15;
+console.log(x)
+
+var x="vallen"
+x+=15;
+console.log(x)
